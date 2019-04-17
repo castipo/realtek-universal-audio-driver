2a8f5718b43230668b07717809b1f036a1f89b71
alanfox2000 <41856670+alanfox2000@users.noreply.github.com>
2019-04-17 21:43:27 +0800

    commit

--- Tool/DISM_INSTALL.bat
+++ Tool/DISM_INSTALL.bat
@@ -19,4 +19,4 @@ FOR /R %~dp0 %%# in (*VCLibs*x64*.appx) DO (
 SET APPXVCLibs=%%~nx#
 )
  
-dism /online /Add-ProvisionedAppxPackage /PackagePath:"%~dp0%APPXBUNDLE%" /LicensePath:"%~dp0%License1XML%" /DependencyPackagePath:"%~dp0%appx%"
+dism /online /Add-ProvisionedAppxPackage /PackagePath:"%~dp0%APPXBUNDLE%" /LicensePath:"%~dp0%License1XML%" /DependencyPackagePath:"%~dp0%APPXVCLibs%"
