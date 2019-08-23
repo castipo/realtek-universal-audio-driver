557172f03421e6812a2c5100f35ee0151b036ad0
alanfox2000 <41856670+alanfox2000@users.noreply.github.com>
2019-08-23 12:31:24 +0800

    commit

---
+++ Verify.bat
@@ -0,0 +1,14 @@
+@echo off
+>>output.txt (
+@echo off
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaAcerConfig.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaAsusConfig.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaClevoConfig.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaFujitsuConfig.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaHpConfigRTK.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaHpConfigRTK2.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaHpConfigRTKDefaultTFName.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaHpConfigRTKNoTF.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaMSIConfig.inf"
+"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\alexaconfig.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\AlexaCfgExt_8777.1\AlexaSamsungConfigRTK.inf"
+)
