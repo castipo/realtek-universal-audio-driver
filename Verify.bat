90a343ff77d45b5ebc594853433681ab74b99ae0
alanfox2000 <41856670+alanfox2000@users.noreply.github.com>
2019-09-06 11:51:51 +0800

    commit

--- Verify.bat
+++
@@ -1,9 +0,0 @@
-@echo off
->>output.txt (
-@echo off
-"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\hdxrt.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\HDXFTS.inf"
-"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\hdxrt.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\HDXLVSST.inf"
-"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\hdxrt.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\HDXPanasonic.inf"
-"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\hdxrt.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\RTAIODAT.DAT"
-"R:\GitHub\realtek-universal-audio-driver\signtool\signtool.exe" verify /c "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\hdxrt.cat" "R:\GitHub\realtek-universal-audio-driver\UAD\Realtek\Codec_8789.1\RTKVHD64.sys"
-)
