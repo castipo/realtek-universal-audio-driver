a43c11196e5a29354fe151d6efb944c79a7421f5
alanfox2000 <41856670+alanfox2000@users.noreply.github.com>
2019-12-04 17:27:21 +0800

    comit

--- Tool/Remove-UAD-Appx.ps1
+++ Tool/Remove-UAD-Appx.ps1
@@ -1,14 +1,14 @@
 #
 # Remove all Realtek Audio Console and related third party UWP apps
 #
-# Revision: 25-11-2018
+# Revision: 04-12-2019
 #
 # Author: alanfox2000
 #
 # https://github.com/alanfox2000/realtek-universal-audio-driver/raw/master/Remove-UAD-Appx.ps1
 #
 
-($AppsList = "RealtekSemiconductorCorp.RealtekAudioControl","RealtekSemiconductorCorp.HPAudioControl","AD2F1837.BangOlufsenAudioControl","AD2F1837.BOAudioControl","AD2F1837.HPAudioCenter","AD2F1837.OMENAudioControl","CreativeTechnologyLtd.SoundBlasterConnect","DolbyLaboratories.DolbyAtmos","DolbyLaboratories.DolbyAtmosSpeakerSystem","DolbyLaboratories.DolbyAudio","DolbyLaboratories.DolbyAudioPremium","DolbyLaboratories.DolbyAudioSpeakerSystem","DolbyLaboratories.DolbyAtmosforGaming","A-Volute.SonicStudio3","A-Volute.28054DF1F58B4","A-Volute.Nahimic","DellInc.AlienwareSoundCenter_htrsf667h5kn2","WavesAudio.WavesMaxxAudioProforDell","WavesAudio.WavesMaxxAudioForFujitsu","WavesAudio.20761030F5EAC","WavesAudio.MaxxAudioProforDell2019","DTSInc.DTSAudioProcessing","DTSInc.DTSCustomforAsus","DTSInc.DTSHeadphoneXv1","DTSInc.DTSStudioSound","DTSInc.DTSSound")
+($AppsList = "RealtekSemiconductorCorp.RealtekAudioControl","RealtekSemiconductorCorp.HPAudioControl","AD2F1837.BangOlufsenAudioControl","AD2F1837.BOAudioControl","AD2F1837.HPAudioCenter","AD2F1837.OMENAudioControl","CreativeTechnologyLtd.SoundBlasterConnect","DolbyLaboratories.DolbyAtmos","DolbyLaboratories.DolbyAtmosSpeakerSystem","DolbyLaboratories.DolbyAudio","DolbyLaboratories.DolbyAudioPremium","DolbyLaboratories.DolbyAudioSpeakerSystem","DolbyLaboratories.DolbyAtmosforGaming","DolbyLaboratories.DolbyAccessOEM","A-Volute.SonicStudio3","A-Volute.28054DF1F58B4","A-Volute.Nahimic","DellInc.AlienwareSoundCenter_htrsf667h5kn2","WavesAudio.WavesMaxxAudioProforDell","WavesAudio.WavesMaxxAudioForFujitsu","WavesAudio.20761030F5EAC","WavesAudio.MaxxAudioProforDell2019","WavesAudio.MaxxAudioProforDell2020","DTSInc.DTSAudioProcessing","DTSInc.DTSCustomforAsus","DTSInc.DTSHeadphoneXv1","DTSInc.DTSStudioSound","DTSInc.DTSSound","DTSInc.DTSXUltra","CRIMiddleware.5929824F64BA4")
 
 ForEach ($App in $AppsList)
 {
